6db510b1ade25427577c93e46c0ce1a4331224bc
Blake <braymondh@gmail.com>
2020-09-12 21:28:48 -0400

    firebase-integration: Added firebase pods to the project

---
+++ Pods/Target Support Files/Pods-Meowist/Pods-Meowist-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Meowist : NSObject
+@end
+@implementation PodsDummy_Pods_Meowist
+@end
---
+++ Pods/Target Support Files/Pods-MeowistTests/Pods-MeowistTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MeowistTests : NSObject
+@end
+@implementation PodsDummy_Pods_MeowistTests
+@end
